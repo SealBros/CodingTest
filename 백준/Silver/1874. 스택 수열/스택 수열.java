50862f52b1fb330ccc3e050d2006f0ee7aabf9c3
SealBros <122066277+SealBros@users.noreply.github.com>
2024-02-21 23:25:02 +0900

    [Silver II] Title: 스택 수열, Time: 1016 ms, Memory: 111308 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int n = in.nextInt();
+        int start = 0;
+
+        for (int i = 0; i < n; i++) {
+            int value = in.nextInt();
+
+            if (value > start) {
+                for (int j = start + 1; j <= value; j++) {
+                    stack.push(j);
+                    sb.append('+').append('\n');
+                }
+                start = value;
+            } else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
